4395f09d33c601ac3146a4c1c3c47e5f75eae4ab
Leonardo2506 <maggical2014.lf@gmail.com>
2021-05-12 15:42:12 -0300

    Initial commit

---
+++ Actividad2/Cliente.cs
@@ -0,0 +1,45 @@
+using System; 
+using System.Collections.Generic;
+using System.Text;
+
+namespace  Actividad2
+{
+    public class Cliente
+    {
+        public string Nombre {get; set;}
+
+        public string Apellido {get; set;}
+
+        public double Efectivo {get; private set;} = 0;
+
+        public Cuenta Cuenta { get; set; }
+
+        public Cliente()
+        {
+           Cuenta = new Cuenta(); 
+        }
+
+        public double Saldo => Efectivo + Cuenta.saldo;
+
+        public int Cbu()
+        {
+            return Cuenta.Cbu; 
+        }
+
+        public void Acreditar(double monto) 
+        {
+            Efectivo = Efectivo + monto * 0.8;
+
+            Cuenta.acreditar(monto * 0.2);
+        } 
+
+        public void Debitar(double monto)
+        {
+            Efectivo = Efectivo - monto * 0.8;
+
+            Cuenta.debitar(monto * 0.2);
+        }
+
+        public bool TieneAlMenos(double monto) => Saldo >= monto;
+    }
+}
---
+++ Actividad2/Cuenta.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Actividad2
+{
+    public class Cuenta
+    { 
+        private static int Contador {get; set;} = 0;
+
+        public int Cbu {get; set;} = 0;
+        
+        public double saldo {get; set;} = 0; 
+    
+       public Cuenta()
+       { 
+            Contador ++;
+
+            Cbu = Contador;
+
+            saldo = 0;
+              
+       }
+
+        public void acreditar(double monto) 
+        {
+            saldo = saldo + monto; 
+        }
+
+        public void debitar(double monto)
+        {
+            saldo = saldo - monto; 
+        }
+
+        public bool TieneAlMenos(double monto) => saldo >= monto;
+
+    }  
+
+}
+        
+    
+    
+    
+
